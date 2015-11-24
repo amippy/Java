0f79fb7e999de5c2c415946591b62e8c53aed4d2
amippy <aminaka1223@gmail.com>
2015-11-24 11:54:12 +0900

    文章と計算⑵

--- Main.java
+++ Main.java
@@ -5,6 +5,12 @@ public class Main {
     System.out.println("生美好き");
     System.out.println("３１+３１の計算するよ！");
     System.out.println(31+31);
+    System.out.println(35-10);
+    System.out.println(-5*2);
+    System.out.println(6*6*3.14);
+    System.out.println("答えは"+64);
+
+
   }
 }
 
