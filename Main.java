89a12036d81a926ec7c00946eb76fe11f67d4701
amippy <aminaka1223@gmail.com>
2015-11-24 11:47:49 +0900

    文章と計算⑴

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
   public static void main (String[] args) {
-    System.out.println("Hello World");
+    System.out.println("あみっぴ");
+    System.out.println("げんきだよ！");
+    System.out.println("生美好き");
+    System.out.println("３１+３１の計算するよ！");
+    System.out.println(31+31);
   }
 }
 
