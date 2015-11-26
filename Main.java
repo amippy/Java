700fa1588eea60375b6ca5cd426a24b876b7152b
amippy <aminaka1223@gmail.com>
2015-11-26 12:13:54 +0900

    文字列の結合

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 public class Main {
   public static void main (String[] args){
-    double d = 8.5 /2;
-    long l =5+2L;
-    System.out.println(d);
-    System.out.println(l);
-
+    String msg ="私の年齢は"+20;
+    System.out.println(msg);
 
   }
 }
