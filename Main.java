c1ac8658f4c01d860c0f5dd95ae40c738b14d564
amippy <aminaka1223@gmail.com>
2015-11-26 11:13:38 +0900

    練習問題１−２

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 public class Main {
   public static void main (String[] args) {
-    final double TAX = 1.08;
-    int fax =5;
-    System.out.println("５万円から４万円に値下げします。");
-    fax = 4;
-    System.out.println("FAXの新価格（税込）");
-    System.out.println(fax*TAX+"万円");
+    int a = 3; int b = 5;
+    int c = a*b;
+    System.out.println("縦幅３横幅５の長方形の面積は"+c);
   }
 }
 
