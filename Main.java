1ace4e1b148b459fc17a65762ee3168ba9a7a6f5
amippy <aminaka1223@gmail.com>
2015-11-26 12:45:20 +0900

    乱数

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
   public static void main (String[] args){
-  String age ="20";
-  int n = Integer.parseInt(age);
-  System.out.println("あなたは来年、"+(n+1)+"歳になるね。");
+  int r = new java.util.Random().nextInt(90);
+  System.out.println("あなたはたぶん"+r+"歳ですね？");
   }
 }
