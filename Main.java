7a6b9f70b4257ba9e3c76feb238fa279f970bb50
amippy <aminaka1223@gmail.com>
2015-11-26 10:48:45 +0900

    定数の利用

--- Main.java
+++ Main.java
@@ -1,16 +1,9 @@
 public class Main {
   public static void main (String[] args) {
-    System.out.println("あみっぴ");
-    System.out.println("げんきだよ！");
-    System.out.println("生美好き");
-    System.out.println("３１+３１の計算するよ！");
-    System.out.println(31+31);
-    System.out.println(35-10);
-    System.out.println(-5*2);
-    System.out.println(6*6*3.14);
-    System.out.println("答えは"+64);
-
-
+    int age =20;
+    System.out.println("私の年齢は"+age);
+    age =31;
+    System.out.println("...いや本当の年齢は"+age);
   }
 }
 
