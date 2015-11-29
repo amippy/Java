017ba2d658ccf5d68b5e82fe12224269e979c8f9
amippy <aminaka1223@gmail.com>
2015-11-29 16:54:41 +0900

    for文のループを２重にして九九の表を完成させる

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
-  public static void main (String[] args){
-    for(int i=0;i<10;){
-    System.out.print("現在"+(i+1)+"周目→");
-  }
+  public static void main(String[] args){
+    for(int i= 1;i<10;i++){
+      for (int j= 1; j<10;j++){
+        System.out.print(i*j);  //掛け算の結果
+        System.out.print(" "); 
+      }
+      System.out.println("");  //改行を出力
+    }
   }
 }
 
