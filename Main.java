c2b88b0db4f2600b8903ce8e374e0ce66719bc1b
amippy <aminaka1223@gmail.com>
2015-11-29 16:34:55 +0900

    for文のループ変数の内容を表示する

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 public class Main {
   public static void main (String[] args){
-  for (int ami = 0; ami<5;ami++){
-    System.out.println("happy!");
+  for (int i=0;i<3;i++){
+    System.out.print("現在"+(i+1)+"周目→");
   }
   }
-
-
-  }
+}
 
