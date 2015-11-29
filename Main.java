12a23f4f767a8e7b2c2494aab1c4997fdfcb6a35
amippy <aminaka1223@gmail.com>
2015-11-29 17:15:58 +0900

    練習問題3.9

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 public class Main {
   public static void main(String[] args){
-    for(int i= 1;i<10;i++){
-      for (int j= 1; j<10;j++){
-        System.out.print(i*j);  //掛け算の結果
-        System.out.print(" "); 
-      }
-      System.out.println("");  //改行を出力
-    }
+  int seibetsu = 1;
+  int age = 20;
+  System.out.println("こんにちは！");
+  if(seibetsu ==0){
+    System.out.println("俺は男だ！");
+  }else{
+    System.out.println("私は女よ！");
+  }
+  if(seibetsu ==0){
+    System.out.println(age+"歳です。");
+  }
+    System.out.println("どうぞ宜しくお願いします！");
   }
 }
 
