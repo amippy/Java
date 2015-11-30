82c188357655dfb50d0a732188c81719ed135eee
amippy <aminaka1223@gmail.com>
2015-11-30 14:45:21 +0900

    メソッドの呼び出し

--- Main.java
+++ Main.java
@@ -1,22 +1,12 @@
 public class Main {
   public static void main(String[] args){
-    int[] numbers={3,4,9};
-    System.out.println("１桁の数字を入力してください。");
-    int input=new java.util.Scanner(System.in).nextInt();
-    for(int n:numbers){
-      if(n==input){
-        System.out.println("あたり！");
-      }
-    }
+    System.out.println("メソッドを呼び出します。");
+    hello();
+    System.out.println("メソッドの呼び出しが終わりました。");
+  }
+  public static void hello(){
+    System.out.println("こんにちは！アミデスーー！");
   }
 }
-
-
-
-    
-
-
-
-
 
 
