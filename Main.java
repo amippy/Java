7ee2aa5e56ff12b6c6d0f9abaad7d96b3926b2dc
amippy <aminaka1223@gmail.com>
2015-11-30 13:37:53 +0900

    配列とfor文

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(String[] args){
-  int[] score = new int[5];
-  System.out.println(score[0]);
+    int [] score = {20,30,40,50,80};
+    for(int i =0; i<score.length;i++){
+      System.out.println(score[i]);
+    }
   }
 }
 
