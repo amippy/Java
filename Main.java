d83f2801164002992468afb3c25f72707ea98aec
amippy <aminaka1223@gmail.com>
2015-11-30 13:55:54 +0900

    ガベージコレクション

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
   public static void main(String[] args){
-  int[]a={1,2,3};
-  int[]b;
-  b=a;
-  b[0]=100;
-  System.out.println(a[0]);
+    boolean b = true;
+    if(b==true){
+      int[]i={1,2,3};
+    }
   }
- }
+}
+
 
 
 
