04a45f8c21acbd71dc7d527ee66b625d35077c8c
amippy <aminaka1223@gmail.com>
2015-11-30 13:52:02 +0900

    配列の舞台裏

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 public class Main {
   public static void main(String[] args){
-    int [] score ={20,30,40,50,80};
-    for (int value:score){
-      System.out.println(value);
-    }
+  int[]a={1,2,3};
+  int[]b;
+  b=a;
+  b[0]=100;
+  System.out.println(a[0]);
   }
-}
+ }
+
+
 
