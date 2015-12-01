9cc084322d3c74e57f293c9a163e00fda92e6176
amippy <aminaka1223@gmail.com>
2015-12-01 23:50:51 +0900

    引数（複数の渡す値）

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 public class Main{
   public static void main (String[]args){
-    System.out.println("メソッドを呼び出します");
-    hello("アミ");
-    hello("有美");
-    hello("ami");
-    System.out.println("呼び出しが終わりました。");
+    add(100,20);
+    add(200,50);
   }
-  public static void hello(String name){
-    System.out.println(name+"さん、こんにちは！");
+  public static void add(int x, int y){
+    int ans =x+y;
+    System.out.println(x+"+"+y+"="+ans);
   }
 }
+
 
