9fe302ba8e255e298880a7fd2784af35b9b0668c
amippy <aminaka1223@gmail.com>
2015-12-02 17:59:46 +0900

    引数の異なる場合のオーバーロード

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
 public class Main{
-  public static int add(int x,int y){
+  public static int add (int x,int y){
     return x+y;
   }
-  public static double add(double x ,double y){
-    return x+y;
+  public static int add(int x,int y,int z){
+    return x+y+z;
   }
-  public static String add(String x,String y){
-    return x+y;
-  }
-  public static void main(String[]arg){
-    System.out.println(add(10,20));
-    System.out.println(add(3.5,2.7));
-    System.out.println(add("hello","world"));
+  public static void main(String[]args){
+    System.out.println("引数が２つ。10+20="+add(10,20));
+    System.out.println("引数が３つ。10+20+30="+add(10,20,30));
   }
 }
 
