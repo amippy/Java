e895c59e3fdd9b68138860877abc08fc81b20891
amippy <aminaka1223@gmail.com>
2015-12-02 18:05:51 +0900

    配列が引数

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
-public class Main{
-  public static int add (int x,int y){
-    return x+y;
+public class Main {
+  public static void printArray(int[]array){
+    for(int element:array){
+      System.out.println(element);
+    }
   }
-  public static int add(int x,int y,int z){
-    return x+y+z;
-  }
-  public static void main(String[]args){
-    System.out.println("引数が２つ。10+20="+add(10,20));
-    System.out.println("引数が３つ。10+20+30="+add(10,20,30));
+  public static void main (String[]args){
+    int[]array={1,2,3};
+    printArray(array);
   }
 }
 
