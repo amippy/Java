de9e0306ccb2f77930ff1021588cca50646fadbf
amippy <aminaka1223@gmail.com>
2015-12-03 10:44:23 +0900

    練習問題５−１

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main {
-  public static int[] makeArray (int size){
-    int[]newArray= new int[size];
-    for(int i=0;i<newArray.length; i++){
-      newArray[i]=i;
-    }
-    return newArray;
-  }
   public static void main (String[]args){
-    int[]array=makeArray(3);
-    for(int i :array){
-      System.out.println(i);
-    }
+    introduceOneself();
+  }
+  public static void introduceOneself(){
+    String name ="Java";
+    int age =34;
+    double height =169.9;
+    char gender ='男';
+    System.out.println("私の名前は"+name+"です。");
+    System.out.println("年齢は"+age+"歳です。");
+    System.out.println("身長は"+height+"センチです。");
+    System.out.println("性別は"+gender+"です。");
   }
 }
 
