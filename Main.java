297c9871449da10258628778b4af66548a775969
amippy <aminaka1223@gmail.com>
2015-12-03 11:20:07 +0900

    計算機プログラムを分割

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 public class Main {
-  public static void main (String[]args){
-    String title ="お誘い";
-    String address ="jessie@j.com";
-    String text ="こんにちわ！";
-    email(title,address,text);
+  public static void main (String []args){
+   int a =10; int b=2;
+  int total = tasu(a,b);
+ int delta = hiku(a,b);
+System.out.println("足したら"+total+"、引いたら"+delta);
   }
-  public static void email(String title,String address,String text){
-    System.out.println(address+"に、以下のメールをお送りしました。");
-    System.out.println("件名"+title);
-    System.out.println("本文"+text);
+
+  public static int tasu(int a, int b){
+    return(a+b);
+  }
+
+  public static int hiku(int a,int b){
+    return (a-b);
   }
 }
 
