3a632bc2e2bbcd9dddede8c937c17a806cca6597
amippy <aminaka1223@gmail.com>
2015-12-03 11:06:21 +0900

    練習問題５−２

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
 public class Main {
   public static void main (String[]args){
-    introduceOneself();
+    String title ="お誘い";
+    String address ="jessie@j.com";
+    String text ="こんにちわ！";
+    email(title,address,text);
   }
-  public static void introduceOneself(){
-    String name ="Java";
-    int age =34;
-    double height =169.9;
-    char gender ='男';
-    System.out.println("私の名前は"+name+"です。");
-    System.out.println("年齢は"+age+"歳です。");
-    System.out.println("身長は"+height+"センチです。");
-    System.out.println("性別は"+gender+"です。");
+  public static void email(String title,String address,String text){
+    System.out.println(address+"に、以下のメールをお送りしました。");
+    System.out.println("件名"+title);
+    System.out.println("本文"+text);
   }
 }
 
