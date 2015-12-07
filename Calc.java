5010b9caa981600ba803b1ca12779b66244bb3a6
amippy <aminaka1223@gmail.com>
2015-12-07 14:25:39 +0900

    calcapp.mainパッケージに所属させる

--- Calc.java
+++ Calc.java
@@ -1,17 +1,11 @@
+package calcapp.main;
 public class Calc{
   public static void main (String[]args){
-    int a =10; int b =11;
-    int total =tasu(a,b);
-    int delta =hiku(a,b);
+    int a= 10; int b= 2;
+    int total = CalcLogic.tasu(a,b);
+    int delta = CalcLogic.hiku(a,b);
     System.out.println("足したら"+total+"、引いたら"+delta);
   }
-
-  public static int tasu(int a,int b){
-    return(a+b);
-  }
-
-  public static int hiku(int a,int b){
-    return(a-b);
-  }
 }
+
 
