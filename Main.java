2d12dd7ee40496e5ca5ccc0defb09b62245c1143
amippy <aminaka1223@gmail.com>
2015-12-08 16:29:28 +0900

    Heroクラスをインスタンス化し利用するコード

--- Main.java
+++ Main.java
@@ -1,27 +1,7 @@
 public class Main {
   public static void main(String []args){
-    Hero h= new Hero();
-  
-    h.name = "Ami";
-    h.hp = 100;
-    
-    Matango m1 = new Matango();
-    m1.hp = 50;
-    m1.suffix ='A';
-
-    Matango m2 = new Matango();
-    m2.hp =48;
-    m2.suffix ='B';
-
-
-    h.slip();
-    m1.run();
-    m2.run();
-    h.run();
+    Hero h;
+    h = new Hero();
+    h.hp =100;
   }
 }
-
-
-
-
-
