e250bd00d8780033b3481cc4856febdcef13e7c8
amippy <aminaka1223@gmail.com>
2015-12-08 11:25:07 +0900

    インスタンスの生成方法

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
 public class Main {
   public static void main(String []args){
-    System.out.println("勇者よ、この仮想世界に生まれよ！");
-    System.out.println("お化けキノコよ、この仮想世界に生まれよ！");
-    System.out.println("勇者よ、戦え！");
-    System.out.println("お化けキノコよ、逃げろ！");
+    Hero h= new Hero();
   }
 }
+
 
 
