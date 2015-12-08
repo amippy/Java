b83ae88a3bfe8c0be6332f2a5271e09a102081ed
amippy <aminaka1223@gmail.com>
2015-12-08 11:31:36 +0900

    インスタンスのメソッド呼び出し

--- Main.java
+++ Main.java
@@ -2,11 +2,18 @@ public class Main {
   public static void main(String []args){
     Hero h= new Hero();
   
-  h.name = "Ami";
-  h.hp = 100;
-  System.out.println("勇者"+h.name+"を生み出しました！")
- }
+    h.name = "Ami";
+    h.hp = 100;
+    System.out.println("勇者"+h.name+"を生み出しました！")
+
+    h.shit(5);
+    h.slip();
+    h.sit(25);
+    h.run();
+  }
 }
+
+
 
 
 
