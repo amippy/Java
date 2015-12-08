0fe5a4a1ac28711851751932e218bfce5f3aac22
amippy <aminaka1223@gmail.com>
2015-12-08 11:17:51 +0900

    変更

--- Hero.java
+++ Hero.java
@@ -6,7 +6,7 @@ public class Hero{
     this.hp = 100;
     System.out.println(this.name+"は、眠って回復した！");
   }
-  void shit(int sec){
+  void sit(int sec){
     this.hp +=sec;
     System.out.printls(this.name+"は、"+sec+"秒座った！");
     System.out.println("HPが"+sec+"ポイント回復した。");
