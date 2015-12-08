06868abc3a5d5e4d9d6a9c4d8a44b699a1ef38c3
amippy <aminaka1223@gmail.com>
2015-12-08 10:51:36 +0900

    神様のクラス

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
 public class Main {
-  public static void main (String []args){
-   int a =10; int b=2;
-  int total = tasu(a,b);
- int delta = hiku(a,b);
-System.out.println("足したら"+total+"、引いたら"+delta);
-  }
-
-  public static int tasu(int a, int b){
-    return(a+b);
-  }
-
-  public static int hiku(int a,int b){
-    return (a-b);
+  public static void main(String []args){
+    System.out.println("勇者よ、この仮想世界に生まれよ！");
+    System.out.println("お化けキノコよ、この仮想世界に生まれよ！");
+    System.out.println("勇者よ、戦え！");
+    System.out.println("お化けキノコよ、逃げろ！");
   }
 }
+
 
