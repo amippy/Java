b3713758160df3be0c8c6840c7faae852c309b8a
amippy <aminaka1223@gmail.com>
2015-12-14 13:29:36 +0900

    コンストラクタのオーバーロード

--- Hero.java
+++ Hero.java
@@ -1,30 +1,15 @@
 public class Hero{
-  String name;
-  int hp;
-
-  void sleep(){
-    this.hp = 100;
-    System.out.println(this.name+"は、眠って回復した！");
-  }
-  void sit(int sec){
-    this.hp +=sec;
-    System.out.println(this.name+"は、"+sec+"秒座った！");
-    System.out.println("HPが"+sec+"ポイント回復した。");
-  }
-  void slip(){
-    this.hp -=5;
-    System.out.println(this.name+"は、転んだ！");
-    System.out.println("5のダメージ");
-  }
-  void run(){
-    System.out.println(this.name+"は逃げ出した！");
-    System.out.println("GAMEOVER");
-    System.out.println("最終HPは"+this.hp+"でした");
-
-  }
-  Hero(){
-    this.hp =100;
+ public static void Hero(String[]args){ 
+    Hero(String name){
+      this.hp = 100;
+      this.name = name;
+    }
+    Hero(){
+      this.hp = 100;
+      this.name = "ダミー";
+    }
   }
 }
+
 
 
