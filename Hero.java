5ae9635d5d53f8db2c1a4c2fcdad9eecf762ec32
amippy <aminaka1223@gmail.com>
2015-12-14 16:47:25 +0900

    HeroクラスにgetNameメソッドを追加

--- Hero.java
+++ Hero.java
@@ -1,11 +1,7 @@
 public class Hero{
-  String name;
-  int hp;
-  static int money;
-
-  static void setRandomMoney(){
-    Hero.money= (int)(Math.random()*1000);
+  private  String name;
+  
+  public String getName(){
+    return this.name;
   }
 }
-
-
