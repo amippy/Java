d19f1e7edd669e4f7a83d78b4c45fbf505475291
amippy <aminaka1223@gmail.com>
2015-12-14 14:03:50 +0900

    個々のインスタンスから静的フィールドmoneyへアクセスして内容表示

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main {
   public static void main(String[]args){
-    Hero h1 =new Hero("あみ");
-
-    System.out.println(h1.name);
+    Hero h1 =new Hero();
     Hero h2 =new Hero();
-
-    System.out.println(h2.name);
+    Hero.money = 100;
+    System.out.println(Hero.money);
+    System.out.println(H1.money);
+    h1.money =300;
+    System.out.println(h2.money);
   }
 }
 
