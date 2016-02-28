df03509f7d87484d15de79542e6938fc8ff7a127
amippy <aminaka1223@gmail.com>
2016-02-28 11:37:21 +0900

    final型

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
-public class Main {
-  public static void main(String[]args){
-    Hero h1 =new Hero();
-    Hero h2 =new Hero();
-    Hero.money = 100;
-    System.out.println(Hero.money);
-    System.out.println(h1.money);
-    h1.money =300;
-    System.out.println(h2.money);
+public class Main{
+  public static void main (String[] args){
+    final double TAX = 1.08;
+    int  fax = 5;
+    System.out.println("５万円から４万円に値下げします");
+    System.out.println("Faxの新価格（税込み）");
+    System.out.println(fax * TAX +"万円");
   }
 }
-
