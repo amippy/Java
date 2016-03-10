d5b3b73adbfa886b70e82041936abc5f69fb9eee
amippy <aminaka1223@gmail.com>
2016-03-10 13:17:21 +0900

    勇者クラスにメソッド追加

--- Hero.java
+++ Hero.java
@@ -1,7 +1,27 @@
 public class Hero{
-  private  String name;
-  
-  public String getName(){
-    return this.name;
-  }
+	String name;
+	int hp;
+
+	void sleep (){
+		this.hp = 100;
+		System.out.println(this.name + "は、眠って回復した");
+	}
+
+	void sit(int sec){
+		this.hp += sec;
+		System.out.println(this.name + "は、" + sec+ "秒座った!");
+	}
+
+	void slip(){
+		this.hp -= 5;
+		System.out.println(this.name + "は、転んだ！");
+		System.out.println("５のダメージ");
+	}
+
+	void run(){
+		System.out.println(this.name + "は、逃げ出した");
+		System.out.println("GAMEOVER"):
+		System.out.println("最終HPは" + this.hp + "でした");
+	}
 }
+
