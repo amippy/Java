c666b4d90b2af356eba1fae33ce1722d2b92ac19
amippy <aminaka1223@gmail.com>
2016-03-10 13:34:03 +0900

    お化けキノコクラスの定義

--- Matango.java
+++ Matango.java
@@ -1,5 +1,11 @@
-public class Matango {
-  int hp;
-  final int LEVEL =10;
-}
+public clss Matango{
+	int hp;
+	final int LEVEL = 10;
+	char suffix;
+	void run(){
+		System.out.println("お化けキノコ" + this.suffix + "は、逃げ出した！");
+	}
 
+
+
+}
