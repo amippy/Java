62545eea457b159e75a8b530cfb098da4fa0da18
amippy <aminaka1223@gmail.com>
2016-03-10 13:20:29 +0900

    インスタンスのフィールドに値代入

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
-public class Main{
-  public static void main (String[] args){
-    final double TAX = 1.08;
-    int  fax = 5;
-    System.out.println("５万円から４万円に値下げします");
-    System.out.println("Faxの新価格（税込み）");
-    System.out.println(fax * TAX +"万円");
-  }
+pubic class Main{
+	public static void main(String[] args){
+
+		Hero h = new Hero();
+
+		h.name = "よしお";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました！");
+	}
 }
+
