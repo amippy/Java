2696a400d9810422a4ae85fb2f62966611a98a51
amippy <aminaka1223@gmail.com>
2016-03-11 11:55:50 +0900

    Hero型の２つの変数

--- Main.java
+++ Main.java
@@ -1,25 +1,15 @@
 public class Main{
 	public static void main(String[] args){
+		Hero h1;
+		h1 = new Hero();
+		h1.hp = 100;
+		
+		Hero h2;
+		h2 = h1;
+		h2.hp = 200;
 
-		Hero h = new Hero();
+		System.out.println(h1.hp);
 
-		h.name = "よしお";
-		h.hp = 100;
-		System.out.println("勇者" + h.name + "を生み出しました！");
-
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
-
-		Matango m2 = new Matango();
-		m2.hp = 48;
-		m2.suffix = 'B';
-
-
-		h.slip();
-		m1.run();
-		m2.run();
-		h.run();
 
 	}
 }
