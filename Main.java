d4831b3ef957defb10eb24f72368e6e3e876a159
amippy <aminaka1223@gmail.com>
2016-03-11 12:29:12 +0900

    クラス型をメソッド引数や戻り値に用いる

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
 public class Main{
 	public static void main(String[] args){
 
-		Sword s = new Sword();
-		s.name = "炎の剣";
-		s.damage = 10;
+		Hero h1 = new Hero();
+		h1.name = "よしお";
+		h1.hp = 100;
 
-		Hero h = new Hero();
-		h.name = "よしお";
-		h.hp = 100;
-		h.sword = s;
+		Hero h2 = new Hero();
+		h2.name = "あきお";
+		h2.hp = 100;
 
-		System.out.println("現在の武器は" + h.sword.name);
+		Wizard w = new Wizard();
+		w.heal(h1);
+		w.heal(h2);
+		w.heal(h2);
+
 	}
 }
 
