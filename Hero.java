c60ce1bf47c83127ec6e2c7c67c95443d45af20a
amippy <aminaka1223@gmail.com>
2016-03-11 12:11:06 +0900

    Sword型をフィールドに利用する

--- Hero.java
+++ Hero.java
@@ -1,27 +1,14 @@
 public class Hero{
 	String name;
 	int hp;
+	Sword sword;
 
-	void sleep (){
-		this.hp = 100;
-		System.out.println(this.name + "は、眠って回復した");
+	void attack(){
+
+		System.out.println(this.name + "は攻撃をした!");
+		System.out.println("敵に５ポイントのダメージを与えた");
+
 	}
 
-	void sit(int sec){
-		this.hp += sec;
-		System.out.println(this.name + "は、" + sec+ "秒座った!");
-	}
-
-	void slip(){
-		this.hp -= 5;
-		System.out.println(this.name + "は、転んだ！");
-		System.out.println("５のダメージ");
-	}
-
-	void run(){
-		System.out.println(this.name + "は、逃げ出した");
-		System.out.println("GAMEOVER");
-		System.out.println("最終HPは" + this.hp + "でした");
-	}
 }
 
