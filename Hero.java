c5f8b3e49e85a377332b3d708d114290908bacdc
amippy <aminaka1223@gmail.com>
2016-03-14 11:01:03 +0900

    setterメソッドの中で値の妥当性をチェックする

--- Hero.java
+++ Hero.java
@@ -1,25 +1,21 @@
 public class Hero{
-	String name;
-	private int hp;
-	Sword sword;
-	static int money;
-
-	void sleep(){
-		this.hp = 100;
-		System.out.println(this.name + "は、眠って回復した");
-	}
-
-	private void die(){
-		System.out.println(this.name + "は、死んでしまった！");
-		System.out.println("GAME OVER です");
-	}
-	public void attack(Matango m){
-		System.out.println(this.name + "は、攻撃を受けた");
-		hp -= 2;
-	}
-
+	public static void main(String[] args){
+	
+	private String name;
 	public void setName(String name){
+		if(name == null){
+			throw new IllegalArgumentException
+			("名前がnullである。処理を中断");
+		}
+		if (name.length() <= 1){
+			throw new IllegalArgumentException
+			("名前が短すぎる。処理を中断");
+		}
+		if(name.length() >= 8){
+			("名前が長すぎる。処理を中断");
+		}
 		this.name = name;
+	}
 	}
 
 }
