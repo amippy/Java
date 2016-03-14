e9297d6852efe04e0a2600102907ed668736ff7a
amippy <aminaka1223@gmail.com>
2016-03-14 11:52:01 +0900

    １０章練習問題２

---
+++ Wand.java
@@ -0,0 +1,22 @@
+public class Wand{
+	private String name;
+	private double power;
+
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public double getPower(){
+		return this.Power;
+	}
+
+	public void setPower(double power){
+		this.Power = Power;
+	}
+		
+}
+
--- Wizard.java
+++ Wizard.java
@@ -1,9 +1,47 @@
 public class Wizard{
-	String name;
-	int hp;
+	private int hp;
+	private int mp;
+	private String name;
+	private Wand wand;
 
-	void heal(Hero h){
-		h.hp += 10;
-		System.out.println(h.name + "のHPを１０回復した!");
+	public int getHp(){
+		return this.hp;
 	}
+	public void setHp(int mp){
+		this.hp = hp;
+	}
+
+	public int getMp(){
+		return this.mp;
+	}
+	public void setMp(int mp){
+		this.mp = mp;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public Wand getWand(){
+		return.this.wand;
+	}
+	public void setWand(Wand wand){
+		this.wand = wand;
+	}
+
+
+
+
+	public void heal(Hero h){
+		int basePoint = 10;
+		int recovPoint = (int) (basePoint * this.wand.power);
+	}
+
+	h.setHp(h.getHp() + recovPoint();
+	System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
 }
+
+
