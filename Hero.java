d94dde03e4104351003df5089c92d9815ba3ffcd
amippy <aminaka1223@gmail.com>
2016-03-14 12:20:00 +0900

    Heroクラスを継承してSuperHeroクラスを作成する

--- Hero.java
+++ Hero.java
@@ -1,21 +1,14 @@
 public class Hero{
-	public static void main(String[] args){
-	
-	private String name;
-	public void setName(String name){
-		if(name == null){
-			throw new IllegalArgumentException
-			("名前がnullである。処理を中断");
-		}
-		if (name.length() <= 1){
-			throw new IllegalArgumentException
-			("名前が短すぎる。処理を中断");
-		}
-		if(name.length() >= 8){
-			("名前が長すぎる。処理を中断");
-		}
-		this.name = name;
-	}
+	private String name = "よしお";
+	private int hp = 100;
+
+	public void attack(Matango m){
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージを与えた");
 	}
 
+	public void run(){
+		System.out.println(this.name + "は、逃げ出した!");
+	}
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String[] args){
-		Hero.h = new Hero();
-		h.setName("");
+		SuperHero sh = new SuperHero();
+		sh.run();
 	}
 }
