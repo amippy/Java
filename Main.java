54a7f26fb518f002595fe5d0a0b686fe6115f1c9
amippy <aminaka1223@gmail.com>
2016-03-22 11:59:44 +0900

    メソッド呼び出し（毎回変数名忘れがち）

--- Main.java
+++ Main.java
@@ -7,6 +7,11 @@ public class Main{
 		System.out.println("勇者" + h.name + "を生み出した！");
 		System.out.println(h.name + "の体力は" + h.hp + "だ！");
 
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
+
 
 
 
