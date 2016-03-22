2ed7f501750f35eb9f9e44f1b2d6f2e28f6dd928
amippy <aminaka1223@gmail.com>
2016-03-22 14:27:06 +0900

    コンストラクタのオーバーロード（復習）

--- Hero.java
+++ Hero.java
@@ -2,41 +2,14 @@ public class Hero{
 
 	String name;
 	int hp;
-	Sword sword;
 
-
-
-	void attack(){
+	Hero(){
 		this.hp = 100;
-		System.out.println(this.name + "は、" + this.sword.name + "を使って攻撃をした！");
-	}
-
-	void run(){
-		this.hp =100;
-		System.out.println(this.name + "は、逃げ出した！");
-	}
-
-	void sit(int sec){
-		this.hp += sec;
-		System.out.println(sec + "秒座った！" + this.name + "は、座った分回復した！"); 
-
-		
-	}
-
-	void slip(){
-		this.hp -= 5;
-		System.out.println(this.name + "は、転んで体力を５減らした");
-	}
-
-	void sleep(){
-		this.hp =100;
-		System.out.println(this.name + "は、寝て回復をした！");
+		this.name = "No Data";
 	}
 
 	Hero(String name){
 		this.hp = 100;
 		this.name = name;
 	}
-
-	
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
 public class Main{
 	public static void main(String[] args){
 
-		Hero h = new Hero("Yoshio");
+		Hero h1 = new Hero("Yoshio");
 
-		System.out.println(h.hp);
-		System.out.println(h.name);
+		System.out.println(h1.hp);
+		System.out.println(h1.name);
+
+		System.out.println("ようこそ！" + h1.name + "さん！");
+
+		Hero h2 = new Hero();
+
+		System.out.println(h2.hp);
+		System.out.println(h2.name);
+
+		System.out.println("名前を入力してください！");
 
 
 	}
