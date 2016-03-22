5005462c240b2fa1d6f973d2b80c1dcd7e1e3c11
amippy <aminaka1223@gmail.com>
2016-03-22 11:54:46 +0900

    インスタンスの学習

--- Hero.java
+++ Hero.java
@@ -1,14 +1,32 @@
 public class Hero{
+
 	String name;
 	int hp;
 
-	Hero(String name){
+	void attack(){
 		this.hp = 100;
-		this.name =  name;
+		System.out.println(this.name + "は、攻撃をした！");
 	}
 
-	Hero(){
-		this.hp = 100;
-		this.name = "ダミー";
+	void run(){
+		this.hp =100;
+		System.out.println(this.name + "は、逃げ出した！");
+	}
+
+	void sit(int sec){
+		this.hp += sec;
+		System.out.println(sec + "秒座った！" + this.name + "は、座った分回復した！"); 
+
+		
+	}
+
+	void slip(){
+		this.hp -= 5;
+		System.out.println(this.name + "は、転んで体力を５減らした");
+	}
+
+	void sleep(){
+		this.hp =100;
+		System.out.println(this.name + "は、寝て回復をした！");
 	}
 }
--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 public class Main{
 	public static void main(String[] args){
+		Hero h = new Hero();
+		h.name = "Yoshio";
+		h.hp = 100;
 
-		Hero h1 = new Hero("よしお");
+		System.out.println("勇者" + h.name + "を生み出した！");
+		System.out.println(h.name + "の体力は" + h.hp + "だ！");
 
-		System.out.println("勇者" + h1.name + "が現れた!");
 
-		Hero h2 = new Hero();
 
-		System.out.println("私の名前は" + h2.name +"よーん");
+
 	}
 }
