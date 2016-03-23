54a54359948c69097c38e0c9ddfb21417b843c6a
amippy <aminaka1223@gmail.com>
2016-03-23 14:42:41 +0900

    親インスタンス（復習）

--- SuperHero.java
+++ SuperHero.java
@@ -15,4 +15,11 @@ public class SuperHero extends Hero{
 	public void run(){
 		System.out.println("撤退した！");
 	}
+
+	public void attack(Matango m){
+		super.attack(m);
+		if(this.flying){
+			super.attack(m);
+		}
+	}
 }
