85672492a9340c98625f26632e39bea027b34d42
amippy <aminaka1223@gmail.com>
2016-03-23 15:09:18 +0900

    内部インスタンスのコンストラクタ引数を指定する

--- Hero.java
+++ Hero.java
@@ -1,36 +1,6 @@
 public class Hero{
 
-	private String name;
-	
-	public String getName(){
-		return this.name;
+	public Hero(){
+		System.out.println("Heroのコンストラクタが動作");
 	}
-
-	public void setName(String name){
-		this.name = name;
-	}
-
-	private int hp;
-
-	Hero(){
-		this.hp =100;
-	}
-
-	public void attack(Matango m){
-		System.out.println(this.name + "の攻撃！");
-		m.hp -= 5;
-		System.out.println("５ポイントのダメージを与えた!");
-	}
-
-	public void run(){
-		System.out.println(this.name + "は、逃げ出した！");
-	}
-
-	public final void slip(){
-		this.hp -= 5;
-		System.out.println(this.name + "は転倒した！");
-		System.out.println(this.name + "は、体力を５減少！");
-	}
-
-
 }
--- SuperHero.java
+++ SuperHero.java
@@ -1,25 +1,8 @@
 public class SuperHero extends Hero{
 
-	private boolean flying;
+	public SuperHero(){
+		super();
+		System.out.println("SuperHeroが生成されます！`");
 
-	public void fly(){
-		this.flying = true;
-		System.out.println("飛び上がった！！");
-	}
-
-	public void land(){
-		this.flying = false;
-		System.out.println("着地した！！");
-	}
-
-	public void run(){
-		System.out.println("撤退した！");
-	}
-
-	public void attack(Matango m){
-		super.attack(m);
-		if(this.flying){
-			super.attack(m);
-		}
 	}
 }
