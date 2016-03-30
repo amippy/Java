db34af0e725eaaf466459a33117b34e2b30afb7c
amippy <aminaka1223@gmail.com>
2016-03-30 10:22:17 +0900

    継承（復習）

--- Hero.java
+++ Hero.java
@@ -1,6 +1,14 @@
 public class Hero{
+	private String name = "よしお";
+	private int hp = 100;
 
-	public Hero(){
-		System.out.println("Heroのコンストラクタが動作");
+	public void attack(Matango m){
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= 5;
+		System.out.println("５ポイントのダメージを与えた！");
+	}
+
+	public void run(){
+		System.out.println(this.name + "は、逃げ出した！");
 	}
 }
