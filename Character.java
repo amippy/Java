deebc5be4faa665f760dd29335069f074bf0b156
amippy <aminaka1223@gmail.com>
2016-03-30 12:08:09 +0900

    抽象クラス

--- Character.java
+++ Character.java
@@ -1,8 +1,11 @@
-public class Character{
+public abstract class Character{
 
 	String name;
 	int hp;
 
-}
+	public void run(){
+		System.out.println(this.name + "は、逃げ出した！");
+	}
+
 	public abstract void attack(Matango m);
 }
