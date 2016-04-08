cc985e42d42216699843820448a2949c5f272830
amippy <aminaka1223@gmail.com>
2016-04-08 16:30:51 +0900

    インスタンスの独立性

--- Main.java
+++ Main.java
@@ -1,15 +1,12 @@
 public class Main{
 	public static void main(String[]args){
-		Hero h = new Hero();
-		h.name = "ジェシー";
-		h.hp = 100;
+		Hero h1 = new Hero();
+		h1.name = "ジェシー";
+		h1.hp = 100;
 
-		Matango m1 = new Matango();
-		m1.hp = 50;
-		m1.suffix = 'A';
-
-		m1.run();
-		h.run();
-
+		Hero h2;
+		h2 = h1;
+		h2.hp = 200;
+		System.out.println(h1.hp);
 	}
 }
