101b5cd617c21f3bca202645132312690f6da615
amippy <aminaka1223@gmail.com>
2016-04-12 12:37:20 +0900

    インスタンスを生成しなくても静的フィールドにアクセス可能

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
 public class Main{
 	public static void main(String[] args){
 
-		Hero h1 = new Hero("ジェシー");
-
-		Hero h2 =  new Hero();
 
 		Hero.money = 1000;
 
-		System.out.println(h1.hp);
 		System.out.println(Hero.money);
 
-		h1.money = 600;
-
-		System.out.println(h2.money);
 	}
 }
